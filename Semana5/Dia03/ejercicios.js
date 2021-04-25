9c248a8506fe709175c36b322984a304ee330a54
JCTC <jctovar87@gmail.com>
2021-04-25 14:55:46 -0500

    j

--- Semana5/Dia03/ejercicios.js
+++ Semana5/Dia03/ejercicios.js
@@ -1,6 +1,6 @@
 let hamburguesaP = 5
-let tipoHamburguesa =""
 let hamburguesaG = 9
+let tipoHamburguesa =""
 let precioHamburguesa = 0
 let escogeCantidad = 0
 let total = 0
@@ -8,21 +8,27 @@ let escogeHamburguer = 0
 
 escogeHamburguer = +prompt("Por favor escoja que hamburguesa pedira: 1 para Simple S/5 y 2 para Doble S/9")
 
-if( escogeHamburguer === 1){
-    precioHamburguesa = 5
-    tipoHamburguesa="SIMPLE"
-}else if (escogeHamburguer == 2){
-    precioHamburguesa = 9
-    tipoHamburguesa="DOBLE"
-}else{
-    escogeHamburguer = +prompt("Ingrese un tipo valido de HAMBURGUESA")
-    
-}
+if(escogeHamburguer === 1 || escogeHamburguer === 2){
 
+    if( escogeHamburguer === 1){
+        precioHamburguesa = 5
+        tipoHamburguesa="SIMPLE"
+    }else if (escogeHamburguer == 2){
+        precioHamburguesa = 9
+        tipoHamburguesa="DOBLE"
+    }
+    
 console.log(precioHamburguesa)
 
 escogeCantidad = +prompt("Ahora indique cuantas Hamburguesas va a desear")
-
 total = escogeCantidad * precioHamburguesa
-
 console.log(`En total son ${escogeCantidad} hamburguesas del tipo ${tipoHamburguesa} y seria un total de S/. ${total} soles`)
+    
+} else{
+
+    prompt("Ingrese un tipo valido de HAMBURGUESA")     
+    
+
+}
+
+
