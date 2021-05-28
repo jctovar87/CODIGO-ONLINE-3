51a2c4430b09474bcc4f67e77df7ac9be94cba57
JCTC <jctovar87@gmail.com>
2021-05-27 19:18:26 -0500

    f

--- Sermana10/administrador/src/views/listarproductosView.js
+++ Sermana10/administrador/src/views/listarproductosView.js
@@ -19,8 +19,34 @@ function ListarProductosView(){
 
         <div>
 
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>nombre</th>
+                        <th>Precio</th>
+                        <th>Descripcion</th>
+                        <th>Stock</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                                {productos.map((prod, i) => (
+                        <tr key={i}>
+                            <td>{prod.nombre}</td>
+                            <td>{prod.precio}</td>
+                            <td>{prod.descripcion}</td>
+                            <td>{prod.stock}</td>
+                            <td></td>
+                        </tr>
+            
+                                    
+                                ))}
+            
+                    </tbody>
+            </table>
         </div>
     )
 
 }
+
+export default ListarProductosView
